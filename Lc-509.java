074d4032d00e601b2a9b965b5b31cee4680a1943
Jerlin shabi <138958887+jerlin-shabi@users.noreply.github.com>
2025-07-25 12:14:59 +0530

    Create Lc-509.java

---
+++ Lc-509.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int fib(int n) {
+        if(n == 0 || n== 1) return n;
+
+        int first = 0;
+        int second = 1;
+
+        for(int i=2; i<=n; i++){
+            int res = first + second;
+            first = second;
+            second = res;
+        }
+        return second;
+}
+}
