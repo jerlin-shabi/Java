8857f31f99f0a15454ce6d7622193c71905fa7db
jerlin-shabi <jerlinshabi.cs22@stellamaryscoe.edu.in>
2025-08-05 14:37:05 +0530

    none

--- .dist/.dist/Main.java
+++ .dist/.dist/Main.java
@@ -2,8 +2,9 @@ public class Main
 {
 	public static void main(String[] args) {
 	    int i=1;
+		int sun=4;
 	    while(i<=100){
-	       System.out.println("love u");
+	       System.out.println(sun+i);
 	       i++;
 	    }
 		
