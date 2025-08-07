b9d3e57b9685da26c07fabd3b67f26dd6ea3932d
jerlin-shabi <jerlinshabi.cs22@stellamaryscoe.edu.in>
2025-08-07 14:00:34 +0530

    just a string in name

---
+++ .dist/.dist/Strings.java
@@ -0,0 +1,12 @@
+public class Strings 
+{
+	public static void main(String[] args) {
+		String[] marks=new String[5];
+		marks[3]="shabi";
+		marks[2]="Jerlin";
+		marks[4]="S";
+		marks[1]="G";
+		System.out.println(marks[2]+" "+ marks[3]+" "+marks[4]+" "+ marks[1]);
+	}
+}
+
